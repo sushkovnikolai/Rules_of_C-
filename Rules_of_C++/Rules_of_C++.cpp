4f38e273fc997c48ba2e0d763874a6be6ada8ff3
Nikolay <sushkovnikolai@gmail.com>
2022-09-05 21:09:59 +0300

    lets begin

--- Rules_of_C++/Rules_of_C++.cpp
+++ Rules_of_C++/Rules_of_C++.cpp
@@ -1,7 +1,10 @@
-﻿#include <iostream>
-
-
+﻿#include <iostream> 
+using namespace std;
+const double Pi = 3.1415;
 int main()
 {
-  
+	double r; cin >> r;
+	double S = Pi * r * r;
+	cout << "Площадь круга = " << S
+		<< endl; return 0;
 }
